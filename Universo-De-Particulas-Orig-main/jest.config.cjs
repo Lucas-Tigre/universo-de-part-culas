145bab8552b568997f9e0aa10e953e87b31e45a3
Lucas-Guercheski <168201998+Lucas-Tigre@users.noreply.github.com>
2025-10-26 17:27:49 -0300

    Delete Universo-De-Particulas-Orig-main directory

--- Universo-De-Particulas-Orig-main/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  moduleNameMapper: {
-    '^https://cdn\\.jsdelivr\\.net/npm/@supabase/supabase-js@2/\\+esm$': '<rootDir>/mocks/supabaseClient.js',
-    '^./supabaseService.js$': '<rootDir>/mocks/supabaseService.js'
-  },
-};
